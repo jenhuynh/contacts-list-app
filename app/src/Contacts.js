3e79e77a621e12e40acde7fa17c894034a4fed8a
jenhuynh <jendoubleh@gmail.com>
2021-09-20 12:09:20 -0700

    Add contacts component

---
+++ app/src/Contacts.js
@@ -0,0 +1,25 @@
+// import * React from "react";
+
+// import * as apiClient from "./apiClient"
+
+// const Contacts = () => {
+//     const [contacts, setContacts] = React.useState([]);
+// }
+
+// const loadContacts = async () => setContacts(await apiClient.getContacts());
+
+// React.useEffect(() => {
+//     loadContacts();
+//   }, []);
+
+//   return (
+//       <section>
+//           <ContactList contacts={contacts} />
+//       </section>
+//   )
+
+//   const ContactList = ({ contacts }) => (
+//       <ul>
+//           {contacts.map(({id, name}) => )}
+//       </ul>
+//   )
