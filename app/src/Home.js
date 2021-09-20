21b55ec88a5de96209dbe9cca620a5b347e2b600
jenhuynh <jendoubleh@gmail.com>
2021-09-20 15:53:07 -0700

    Add commented out home component parts and export default change from app to home

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,33 +1,35 @@
 import * as React from "react";
 
-import { Routes, Route, Link } from "react-router-dom";
+// import { Routes, Route, Link } from "react-router-dom";
 
+import Contacts from "./Contacts";
 import Tasks from "./Tasks";
 
-const App = () => (
-  <main>
-    <nav>
-      <Link to="/">Home</Link> | <Link to="dashboard">Dashboard</Link>
-    </nav>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  </main>
-);
+// const App = () => (
+//   <main>
+//     <nav>
+//       <Link to="/">Home</Link> | <Link to="dashboard">Dashboard</Link>
+//     </nav>
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/dashboard" element={<Dashboard />} />
+//     </Routes>
+//   </main>
+// );
 
 const Home = () => (
   <>
     <h1>{process.env.REACT_APP_TITLE}</h1>
     <h2>{process.env.REACT_APP_SUBTITLE}</h2>
     <Tasks />
+    <Contacts />
   </>
 );
 
-const Dashboard = () => (
-  <>
-    <h1>Dashboard</h1>
-  </>
-);
+// const Dashboard = () => (
+//   <>
+//     <h1>Dashboard</h1>
+//   </>
+// );
 
-export default App;
+export default Home;
