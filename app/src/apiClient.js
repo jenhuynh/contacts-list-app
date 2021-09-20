4d38bfee2c222a5c21ce6382d3b2ebef3d6a161a
jenhuynh <jendoubleh@gmail.com>
2021-09-20 15:54:03 -0700

    Add getcontct and createcontact functions

--- app/src/apiClient.js
+++ app/src/apiClient.js
@@ -2,6 +2,30 @@ export const getTasks = () => _get("/api/tasks");
 
 export const addTask = (name) => _post("/api/tasks", { name });
 
+//get and addContact function
+// export const getContacts = () => {
+//   _get("/api/contacts");
+// };
+
+// export const createContact = (name) => _post("/api/contacts", { contact });
+
+//get and addContact function
+export const getContacts = async () => {
+  const response = await fetch("/api/contacts");
+  return response.json();
+};
+
+export const createContact = async (contact) => {
+  const response = await fetch("/api/contacts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(contact),
+  });
+  return response.json();
+};
+
 const _get = async (url) => (await fetch(url)).json();
 
 const _post = async (url, body) => {
