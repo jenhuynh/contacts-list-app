65366a5d93631bc9377aed88b2d43f4841c13c23
jenhuynh <jendoubleh@gmail.com>
2021-09-21 14:55:57 -0700

    Add code for edit and click option

--- app/src/Contacts.js
+++ app/src/Contacts.js
@@ -1,3 +1,67 @@
+// import * as React from "react";
+
+// import ContactCard from "./ContactCard";
+// import CreateContact from "./CreateContact";
+// import * as apiClient from "./apiClient";
+
+// const Contacts = () => {
+//   const [contacts, setContacts] = React.useState([]);
+//   const loadContacts = async () => setContacts(await apiClient.getContacts());
+//   const createContact = (name, email, phone_number, note) => {
+//     apiClient
+//       .createContact(name, email, phone_number, email, note)
+//       .then(loadContacts);
+//   };
+
+//   React.useEffect(() => {
+//     loadContacts();
+//   }, []);
+
+//   return (
+//     <section>
+//       <ContactList {...{ contacts }} />
+//       <CreateContact createContact={createContact} />
+//     </section>
+//   );
+// };
+
+// //lists all data from contact list db
+// const ContactList = ({ contacts }) => {
+//   const [selectedContact, setSelectedContact] = React.useState("");
+//   const [contactDetails, setContactDetails] = React.useState({});
+
+//   const loadContact = React.useCallback(
+//     () => apiClient.getContacts(selectedContact).then(setContactDetails),
+//     [selectedContact],
+//   );
+
+//   React.useEffect(() => {
+//     selectedContact !== undefined && loadContact();
+//   }, [selectedContact, loadContact]);
+
+//   return (
+//     <div>
+//       <ContactCard {...{ selectedContact, contactDetails }} />
+//       <section>
+//         <h1>List of Contacts</h1>
+//         <ul key={contacts.contact_id}>
+//           {contacts.map(({ id, name, email, phone_number, notes }) => (
+//             <li key={id}>
+//               {name}, {email}, {phone_number}, {notes}
+//             </li>
+//           ))}
+//           <button onClick={() => setSelectedContact(contacts.contact_id)}>
+//             Details
+//           </button>
+//         </ul>
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default Contacts;
+
+//*******************Working contact list mapping **************************** */
 import * as React from "react";
 
 import CreateContact from "./CreateContact";
@@ -35,39 +99,5 @@ const ContactList = ({ contacts }) => (
     ))}
   </ul>
 );
-
-// const AddContact = ({ addContact }) => {
-//   const onSubmit = (e) => {
-//     const form = e.currentTarget;
-//     const {
-//       name: { value: name },
-//       email: { value: email },
-//       phone_number: { value: phone_number },
-//       notes: { value: notes },
-//     } = form.elements;
-
-//     e.preventDefault();
-//     addContact({ name, email, phone_number, notes });
-//     form.reset();
-//   };
-
-//   return (
-//     <form onSubmit={onSubmit}>
-//       <label>
-//         Name: <input name="name" required />
-//       </label>
-//       <label>
-//         Email: <input name="email" type="email" required />
-//       </label>
-//       <label>
-//         Phone Number: <input name="phone_number" type="integer" required />
-//       </label>
-//       <label>
-//         Notes: <input name="notes" type="text" required />
-//       </label>
-//       <button>Add user</button>
-//     </form>
-//   );
-// };
 
 export default Contacts;
